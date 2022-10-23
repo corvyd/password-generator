0920e8d8f4b1768b657abee46ffa689ac10db515
alexLares <alexis.lares19@gmail.com>
2022-10-22 23:04:31 -0700

    password-generator

---
+++ script.js
@@ -0,0 +1,47 @@
+var specialcharacters = ["@","%","+","\\","/","'","!","#","$","^","?",":",",",")","(","{","}","[","]","~","-","_","."] ;
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var UpperCase = ["A"] ;
+var LowerCase = ["a"] ;
+
+function getpasswordoptions() {
+  var length = parseInt(
+    prompt("how many characters would you like your password to contain?")
+  );
+  if(Number.isNaN(length)){
+    alert("Password length must be a number");
+    return null;
+  }
+  if(length<8 || length>128){
+    alert("Password length must be between 8 and 128 characters")
+  }
+  var hasSpecial = confirm("click ok if you would like to include special characters in your password")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
